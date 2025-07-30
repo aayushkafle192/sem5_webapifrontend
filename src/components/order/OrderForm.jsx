021aaa4d2c0a1096a423454d999528e551b264df
Aayush kafle <aayushkafle192@gmail.com>
2025-07-30 20:01:23 +0545

    Descriptive message

--- src/components/order/OrderForm.jsx
+++ src/components/order/OrderForm.jsx
@@ -949,7 +949,7 @@
 //                 <input
 //                   type="tex"
 //                   value={formDat.c}
-//                   onChang={e => handleInputChange("city", e.target.value)}
+//                   onChang={e => handleInputChang("city", e.target.value)}
 //                   className={errors.city ? "error" : ""}
 //                 />
 //                 {errors.city && <span className="error-message">{errors.city}</span>}
